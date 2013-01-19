102ba60a806e133cc97aa973505941bd62b10327
tanelteede@gmail.com <tanelteede@gmail.com@98dd96e8-d0ae-4ced-7c27-9964b07faf77>
2013-01-19 09:07:00 +0000

    updated LED logic to be simpler

--- LedBar.Designer.cs
+++ LedBar.Designer.cs
@@ -41,6 +41,19 @@
             this.l3 = new System.Windows.Forms.Label();
             this.l2 = new System.Windows.Forms.Label();
             this.l1 = new System.Windows.Forms.Label();
+            this.lon1 = new System.Windows.Forms.Label();
+            this.lon2 = new System.Windows.Forms.Label();
+            this.lon3 = new System.Windows.Forms.Label();
+            this.lon4 = new System.Windows.Forms.Label();
+            this.lon5 = new System.Windows.Forms.Label();
+            this.lon6 = new System.Windows.Forms.Label();
+            this.lon7 = new System.Windows.Forms.Label();
+            this.lon8 = new System.Windows.Forms.Label();
+            this.lon9 = new System.Windows.Forms.Label();
+            this.lon10 = new System.Windows.Forms.Label();
+            this.lon11 = new System.Windows.Forms.Label();
+            this.lon12 = new System.Windows.Forms.Label();
+            this.lon13 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // l13
@@ -147,11 +160,128 @@
             this.l1.Size = new System.Drawing.Size(14, 3);
             this.l1.TabIndex = 26;
             // 
+            // lon1
+            // 
+            this.lon1.BackColor = System.Drawing.Color.Lime;
+            this.lon1.Location = new System.Drawing.Point(1, 1);
+            this.lon1.Name = "lon1";
+            this.lon1.Size = new System.Drawing.Size(14, 3);
+            this.lon1.TabIndex = 39;
+            // 
+            // lon2
+            // 
+            this.lon2.BackColor = System.Drawing.Color.Lime;
+            this.lon2.Location = new System.Drawing.Point(16, 1);
+            this.lon2.Name = "lon2";
+            this.lon2.Size = new System.Drawing.Size(14, 3);
+            this.lon2.TabIndex = 40;
+            // 
+            // lon3
+            // 
+            this.lon3.BackColor = System.Drawing.Color.Lime;
+            this.lon3.Location = new System.Drawing.Point(31, 1);
+            this.lon3.Name = "lon3";
+            this.lon3.Size = new System.Drawing.Size(14, 3);
+            this.lon3.TabIndex = 41;
+            // 
+            // lon4
+            // 
+            this.lon4.BackColor = System.Drawing.Color.Lime;
+            this.lon4.Location = new System.Drawing.Point(46, 1);
+            this.lon4.Name = "lon4";
+            this.lon4.Size = new System.Drawing.Size(14, 3);
+            this.lon4.TabIndex = 42;
+            // 
+            // lon5
+            // 
+            this.lon5.BackColor = System.Drawing.Color.Lime;
+            this.lon5.Location = new System.Drawing.Point(61, 1);
+            this.lon5.Name = "lon5";
+            this.lon5.Size = new System.Drawing.Size(14, 3);
+            this.lon5.TabIndex = 43;
+            // 
+            // lon6
+            // 
+            this.lon6.BackColor = System.Drawing.Color.Lime;
+            this.lon6.Location = new System.Drawing.Point(76, 1);
+            this.lon6.Name = "lon6";
+            this.lon6.Size = new System.Drawing.Size(14, 3);
+            this.lon6.TabIndex = 44;
+            // 
+            // lon7
+            // 
+            this.lon7.BackColor = System.Drawing.Color.Lime;
+            this.lon7.Location = new System.Drawing.Point(91, 1);
+            this.lon7.Name = "lon7";
+            this.lon7.Size = new System.Drawing.Size(14, 3);
+            this.lon7.TabIndex = 45;
+            // 
+            // lon8
+            // 
+            this.lon8.BackColor = System.Drawing.Color.Lime;
+            this.lon8.Location = new System.Drawing.Point(106, 1);
+            this.lon8.Name = "lon8";
+            this.lon8.Size = new System.Drawing.Size(14, 3);
+            this.lon8.TabIndex = 46;
+            // 
+            // lon9
+            // 
+            this.lon9.BackColor = System.Drawing.Color.Lime;
+            this.lon9.Location = new System.Drawing.Point(121, 1);
+            this.lon9.Name = "lon9";
+            this.lon9.Size = new System.Drawing.Size(14, 3);
+            this.lon9.TabIndex = 47;
+            // 
+            // lon10
+            // 
+            this.lon10.BackColor = System.Drawing.Color.Yellow;
+            this.lon10.Location = new System.Drawing.Point(136, 1);
+            this.lon10.Name = "lon10";
+            this.lon10.Size = new System.Drawing.Size(14, 3);
+            this.lon10.TabIndex = 48;
+            // 
+            // lon11
+            // 
+            this.lon11.BackColor = System.Drawing.Color.Yellow;
+            this.lon11.Location = new System.Drawing.Point(151, 1);
+            this.lon11.Name = "lon11";
+            this.lon11.Size = new System.Drawing.Size(14, 3);
+            this.lon11.TabIndex = 49;
+            // 
+            // lon12
+            // 
+            this.lon12.BackColor = System.Drawing.Color.Yellow;
+            this.lon12.Location = new System.Drawing.Point(166, 1);
+            this.lon12.Name = "lon12";
+            this.lon12.Size = new System.Drawing.Size(14, 3);
+            this.lon12.TabIndex = 50;
+            // 
+            // lon13
+            // 
+            this.lon13.BackColor = System.Drawing.Color.Red;
+            this.lon13.Location = new System.Drawing.Point(181, 1);
+            this.lon13.Name = "lon13";
+            this.lon13.Size = new System.Drawing.Size(14, 3);
+            this.lon13.TabIndex = 51;
+            // 
             // LedBar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Controls.Add(this.lon13);
+            this.Controls.Add(this.lon12);
+            this.Controls.Add(this.lon11);
+            this.Controls.Add(this.lon10);
+            this.Controls.Add(this.lon9);
+            this.Controls.Add(this.lon8);
+            this.Controls.Add(this.lon7);
+            this.Controls.Add(this.lon6);
+            this.Controls.Add(this.lon5);
+            this.Controls.Add(this.lon4);
+            this.Controls.Add(this.lon3);
+            this.Controls.Add(this.lon2);
+            this.Controls.Add(this.lon1);
             this.Controls.Add(this.l13);
             this.Controls.Add(this.l12);
             this.Controls.Add(this.l11);
@@ -186,5 +316,18 @@
         private System.Windows.Forms.Label l3;
         private System.Windows.Forms.Label l2;
         private System.Windows.Forms.Label l1;
+        private System.Windows.Forms.Label lon1;
+        private System.Windows.Forms.Label lon2;
+        private System.Windows.Forms.Label lon3;
+        private System.Windows.Forms.Label lon4;
+        private System.Windows.Forms.Label lon5;
+        private System.Windows.Forms.Label lon6;
+        private System.Windows.Forms.Label lon7;
+        private System.Windows.Forms.Label lon8;
+        private System.Windows.Forms.Label lon9;
+        private System.Windows.Forms.Label lon10;
+        private System.Windows.Forms.Label lon11;
+        private System.Windows.Forms.Label lon12;
+        private System.Windows.Forms.Label lon13;
     }
 }
