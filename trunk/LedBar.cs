1e7efbe069368e2003af751cb3c26d5918a6bf34
tanelteede@gmail.com <tanelteede@gmail.com@98dd96e8-d0ae-4ced-7c27-9964b07faf77>
2013-01-19 09:07:00 +0000

    updated LED logic to be simpler

--- trunk/LedBar.cs
+++ trunk/LedBar.cs
@@ -1,42 +1,27 @@
 ﻿using System;
-using System.Drawing;
 using System.Windows.Forms;
 
 namespace AudioSwitch
 {
     public partial class LedBar : UserControl
     {
-        private readonly Label[] LED;
+        private readonly Label[] LEDon;
         private int lastValue;
-
-        private static readonly Color[] pgOnColors = new[]
-                                                  {
-                                                      Color.Black, Color.Lime, Color.Lime, Color.Lime, Color.Lime,
-                                                      Color.Lime, Color.Lime, Color.Lime, Color.Lime, Color.Lime,
-                                                      Color.Yellow, Color.Yellow, Color.Yellow, Color.Red
-                                                  };
-
-        private static readonly Color[] pgOffColors = new[]
-                                                   {
-                                                       Color.Black, Color.Green, Color.Green, Color.Green, Color.Green,
-                                                       Color.Green, Color.Green, Color.Green, Color.Green, Color.Green,
-                                                       Color.Olive, Color.Olive, Color.Olive, Color.Maroon
-                                                   };
 
         public LedBar()
         {
             InitializeComponent();
-            LED = new[] { new Label(), l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13 };
+            LEDon = new[] { new Label(), lon1, lon2, lon3, lon4, lon5, lon6, lon7, lon8, lon9, lon10, lon11, lon12, lon13 };
         }
 
         public void SetValue(float value)
         {
             var val = (int)Math.Ceiling(value * 14);
             if (lastValue == val) return;
+            lastValue = val;
 
             for (var i = 0; i < 14; i++)
-                LED[i].BackColor = val >= i ? pgOnColors[i] : pgOffColors[i];
-            lastValue = val;
+                LEDon[i].Visible = val >= i;
         }
     }
 }
