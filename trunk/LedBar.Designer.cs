158e913711206fe35e10636df03527defe015465
tanelteede@gmail.com <tanelteede@gmail.com@98dd96e8-d0ae-4ced-7c27-9964b07faf77>
2013-02-03 00:11:58 +0000

    reduced flickering on startup, made LED's higher by 1px :)

--- trunk/LedBar.Designer.cs
+++ trunk/LedBar.Designer.cs
@@ -61,7 +61,7 @@
             this.l13.BackColor = System.Drawing.Color.Maroon;
             this.l13.Location = new System.Drawing.Point(181, 1);
             this.l13.Name = "l13";
-            this.l13.Size = new System.Drawing.Size(14, 3);
+            this.l13.Size = new System.Drawing.Size(14, 4);
             this.l13.TabIndex = 38;
             // 
             // l12
@@ -69,7 +69,7 @@
             this.l12.BackColor = System.Drawing.Color.Olive;
             this.l12.Location = new System.Drawing.Point(166, 1);
             this.l12.Name = "l12";
-            this.l12.Size = new System.Drawing.Size(14, 3);
+            this.l12.Size = new System.Drawing.Size(14, 4);
             this.l12.TabIndex = 37;
             // 
             // l11
@@ -77,7 +77,7 @@
             this.l11.BackColor = System.Drawing.Color.Olive;
             this.l11.Location = new System.Drawing.Point(151, 1);
             this.l11.Name = "l11";
-            this.l11.Size = new System.Drawing.Size(14, 3);
+            this.l11.Size = new System.Drawing.Size(14, 4);
             this.l11.TabIndex = 36;
             // 
             // l10
@@ -85,7 +85,7 @@
             this.l10.BackColor = System.Drawing.Color.Olive;
             this.l10.Location = new System.Drawing.Point(136, 1);
             this.l10.Name = "l10";
-            this.l10.Size = new System.Drawing.Size(14, 3);
+            this.l10.Size = new System.Drawing.Size(14, 4);
             this.l10.TabIndex = 35;
             // 
             // l9
@@ -93,7 +93,7 @@
             this.l9.BackColor = System.Drawing.Color.Green;
             this.l9.Location = new System.Drawing.Point(121, 1);
             this.l9.Name = "l9";
-            this.l9.Size = new System.Drawing.Size(14, 3);
+            this.l9.Size = new System.Drawing.Size(14, 4);
             this.l9.TabIndex = 34;
             // 
             // l8
@@ -101,7 +101,7 @@
             this.l8.BackColor = System.Drawing.Color.Green;
             this.l8.Location = new System.Drawing.Point(106, 1);
             this.l8.Name = "l8";
-            this.l8.Size = new System.Drawing.Size(14, 3);
+            this.l8.Size = new System.Drawing.Size(14, 4);
             this.l8.TabIndex = 33;
             // 
             // l7
@@ -109,7 +109,7 @@
             this.l7.BackColor = System.Drawing.Color.Green;
             this.l7.Location = new System.Drawing.Point(91, 1);
             this.l7.Name = "l7";
-            this.l7.Size = new System.Drawing.Size(14, 3);
+            this.l7.Size = new System.Drawing.Size(14, 4);
             this.l7.TabIndex = 32;
             // 
             // l6
@@ -117,7 +117,7 @@
             this.l6.BackColor = System.Drawing.Color.Green;
             this.l6.Location = new System.Drawing.Point(76, 1);
             this.l6.Name = "l6";
-            this.l6.Size = new System.Drawing.Size(14, 3);
+            this.l6.Size = new System.Drawing.Size(14, 4);
             this.l6.TabIndex = 31;
             // 
             // l5
@@ -125,7 +125,7 @@
             this.l5.BackColor = System.Drawing.Color.Green;
             this.l5.Location = new System.Drawing.Point(61, 1);
             this.l5.Name = "l5";
-            this.l5.Size = new System.Drawing.Size(14, 3);
+            this.l5.Size = new System.Drawing.Size(14, 4);
             this.l5.TabIndex = 30;
             // 
             // l4
@@ -133,7 +133,7 @@
             this.l4.BackColor = System.Drawing.Color.Green;
             this.l4.Location = new System.Drawing.Point(46, 1);
             this.l4.Name = "l4";
-            this.l4.Size = new System.Drawing.Size(14, 3);
+            this.l4.Size = new System.Drawing.Size(14, 4);
             this.l4.TabIndex = 29;
             // 
             // l3
@@ -141,7 +141,7 @@
             this.l3.BackColor = System.Drawing.Color.Green;
             this.l3.Location = new System.Drawing.Point(31, 1);
             this.l3.Name = "l3";
-            this.l3.Size = new System.Drawing.Size(14, 3);
+            this.l3.Size = new System.Drawing.Size(14, 4);
             this.l3.TabIndex = 28;
             // 
             // l2
@@ -149,7 +149,7 @@
             this.l2.BackColor = System.Drawing.Color.Green;
             this.l2.Location = new System.Drawing.Point(16, 1);
             this.l2.Name = "l2";
-            this.l2.Size = new System.Drawing.Size(14, 3);
+            this.l2.Size = new System.Drawing.Size(14, 4);
             this.l2.TabIndex = 27;
             // 
             // l1
@@ -157,7 +157,7 @@
             this.l1.BackColor = System.Drawing.Color.Green;
             this.l1.Location = new System.Drawing.Point(1, 1);
             this.l1.Name = "l1";
-            this.l1.Size = new System.Drawing.Size(14, 3);
+            this.l1.Size = new System.Drawing.Size(14, 4);
             this.l1.TabIndex = 26;
             // 
             // lon1
@@ -165,7 +165,7 @@
             this.lon1.BackColor = System.Drawing.Color.Lime;
             this.lon1.Location = new System.Drawing.Point(1, 1);
             this.lon1.Name = "lon1";
-            this.lon1.Size = new System.Drawing.Size(14, 3);
+            this.lon1.Size = new System.Drawing.Size(14, 4);
             this.lon1.TabIndex = 39;
             this.lon1.Visible = false;
             // 
@@ -174,7 +174,7 @@
             this.lon2.BackColor = System.Drawing.Color.Lime;
             this.lon2.Location = new System.Drawing.Point(16, 1);
             this.lon2.Name = "lon2";
-            this.lon2.Size = new System.Drawing.Size(14, 3);
+            this.lon2.Size = new System.Drawing.Size(14, 4);
             this.lon2.TabIndex = 40;
             this.lon2.Visible = false;
             // 
@@ -183,7 +183,7 @@
             this.lon3.BackColor = System.Drawing.Color.Lime;
             this.lon3.Location = new System.Drawing.Point(31, 1);
             this.lon3.Name = "lon3";
-            this.lon3.Size = new System.Drawing.Size(14, 3);
+            this.lon3.Size = new System.Drawing.Size(14, 4);
             this.lon3.TabIndex = 41;
             this.lon3.Visible = false;
             // 
@@ -192,7 +192,7 @@
             this.lon4.BackColor = System.Drawing.Color.Lime;
             this.lon4.Location = new System.Drawing.Point(46, 1);
             this.lon4.Name = "lon4";
-            this.lon4.Size = new System.Drawing.Size(14, 3);
+            this.lon4.Size = new System.Drawing.Size(14, 4);
             this.lon4.TabIndex = 42;
             this.lon4.Visible = false;
             // 
@@ -201,7 +201,7 @@
             this.lon5.BackColor = System.Drawing.Color.Lime;
             this.lon5.Location = new System.Drawing.Point(61, 1);
             this.lon5.Name = "lon5";
-            this.lon5.Size = new System.Drawing.Size(14, 3);
+            this.lon5.Size = new System.Drawing.Size(14, 4);
             this.lon5.TabIndex = 43;
             this.lon5.Visible = false;
             // 
@@ -210,7 +210,7 @@
             this.lon6.BackColor = System.Drawing.Color.Lime;
             this.lon6.Location = new System.Drawing.Point(76, 1);
             this.lon6.Name = "lon6";
-            this.lon6.Size = new System.Drawing.Size(14, 3);
+            this.lon6.Size = new System.Drawing.Size(14, 4);
             this.lon6.TabIndex = 44;
             this.lon6.Visible = false;
             // 
@@ -219,7 +219,7 @@
             this.lon7.BackColor = System.Drawing.Color.Lime;
             this.lon7.Location = new System.Drawing.Point(91, 1);
             this.lon7.Name = "lon7";
-            this.lon7.Size = new System.Drawing.Size(14, 3);
+            this.lon7.Size = new System.Drawing.Size(14, 4);
             this.lon7.TabIndex = 45;
             this.lon7.Visible = false;
             // 
@@ -228,7 +228,7 @@
             this.lon8.BackColor = System.Drawing.Color.Lime;
             this.lon8.Location = new System.Drawing.Point(106, 1);
             this.lon8.Name = "lon8";
-            this.lon8.Size = new System.Drawing.Size(14, 3);
+            this.lon8.Size = new System.Drawing.Size(14, 4);
             this.lon8.TabIndex = 46;
             this.lon8.Visible = false;
             // 
@@ -237,7 +237,7 @@
             this.lon9.BackColor = System.Drawing.Color.Lime;
             this.lon9.Location = new System.Drawing.Point(121, 1);
             this.lon9.Name = "lon9";
-            this.lon9.Size = new System.Drawing.Size(14, 3);
+            this.lon9.Size = new System.Drawing.Size(14, 4);
             this.lon9.TabIndex = 47;
             this.lon9.Visible = false;
             // 
@@ -246,7 +246,7 @@
             this.lon10.BackColor = System.Drawing.Color.Yellow;
             this.lon10.Location = new System.Drawing.Point(136, 1);
             this.lon10.Name = "lon10";
-            this.lon10.Size = new System.Drawing.Size(14, 3);
+            this.lon10.Size = new System.Drawing.Size(14, 4);
             this.lon10.TabIndex = 48;
             this.lon10.Visible = false;
             // 
@@ -255,7 +255,7 @@
             this.lon11.BackColor = System.Drawing.Color.Yellow;
             this.lon11.Location = new System.Drawing.Point(151, 1);
             this.lon11.Name = "lon11";
-            this.lon11.Size = new System.Drawing.Size(14, 3);
+            this.lon11.Size = new System.Drawing.Size(14, 4);
             this.lon11.TabIndex = 49;
             this.lon11.Visible = false;
             // 
@@ -264,7 +264,7 @@
             this.lon12.BackColor = System.Drawing.Color.Yellow;
             this.lon12.Location = new System.Drawing.Point(166, 1);
             this.lon12.Name = "lon12";
-            this.lon12.Size = new System.Drawing.Size(14, 3);
+            this.lon12.Size = new System.Drawing.Size(14, 4);
             this.lon12.TabIndex = 50;
             this.lon12.Visible = false;
             // 
@@ -273,7 +273,7 @@
             this.lon13.BackColor = System.Drawing.Color.Red;
             this.lon13.Location = new System.Drawing.Point(181, 1);
             this.lon13.Name = "lon13";
-            this.lon13.Size = new System.Drawing.Size(14, 3);
+            this.lon13.Size = new System.Drawing.Size(14, 4);
             this.lon13.TabIndex = 51;
             this.lon13.Visible = false;
             // 
@@ -309,7 +309,7 @@
             this.Controls.Add(this.l2);
             this.Controls.Add(this.l1);
             this.Name = "LedBar";
-            this.Size = new System.Drawing.Size(196, 5);
+            this.Size = new System.Drawing.Size(196, 6);
             this.ResumeLayout(false);
 
         }
