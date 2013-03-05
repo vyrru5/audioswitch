4878574b765ef6fc48c7a88fca85bb031d54bc7f
tanelteede@gmail.com <tanelteede@gmail.com@98dd96e8-d0ae-4ced-7c27-9964b07faf77>
2013-03-05 19:56:27 +0000

    added support for .ico files as device icons.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.41")]
-[assembly: AssemblyFileVersion("1.41")]
+[assembly: AssemblyVersion("1.42")]
+[assembly: AssemblyFileVersion("1.42")]
